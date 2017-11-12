Added new datasets for TSP. Also added total distance calculation (NOT YET CORRECT).

kohonen_network/Trainer.py
@@ -107,4 +107,65 @@ class Trainer():
         return "%s accuracy: %f %s (%d/%d)" %(test_type, correct/len(cases)*100, "%", correct, len(cases))
 
     def test_distance(self, expected):
-        pass
+        def find_closest_cities():
+            cities = []
+            for lat, lng in zip(self.neurons[0], self.neurons[1]):
+                distance = np.array([])
+                for otherlat, otherlng in self.network.inputs:
+                    distance = np.append(distance,np.sqrt(np.power((otherlat-lat), 2) + np.power((otherlng-lng), 2)))
+                cities += [(np.argmin(distance), min(distance))]
+            return cities
+
+        def create_city_tour(cities):
+            city_tour = []
+            for city, distance in cities:
+                for othercity, otherdistance in cities:
+                    if city == othercity and distance > otherdistance:
+                        break
+                else: city_tour += [city]
+            return city_tour
+
+        def validate(city_tour) -> bool:
+            all_cities = list(range(0, self.config.nodes))
+            perfect = True
+            for city in city_tour:
+                if all_cities[city] == -1:
+                    print("DUPLICATE CITY. CITY %d VISITED TWICE!" % city)
+                    perfect = False
+                else: all_cities[city] = -1
+            if any(x > -1 for x in all_cities):
+                print("SALESMAN DID NOT VISIT ALL CITIES.")
+                perfect = False
+            return perfect
+
+        city_tour = create_city_tour(find_closest_cities())
+        if not validate(city_tour):
+            pass
+
+        distance = 0
+        for index in city_tour:
+            city, prevlat, prevlng  = self.config.casemanager.original[index-1]
+            city, lat, lng          = self.config.casemanager.original[index]
+            p1 = np.power( lat - prevlat , 2)
+            p2 = np.power( lng - prevlng , 2)
+            distance += np.sqrt( p1 + p2 )
+        print("TOTAL DISTANCE: %f (optimal: %d)" % (distance, expected))
+        print("Percent of optimal: %f %s" % (float(distance)/float(expected)*100, "%"))
+
+        # visited = []
+        #
+        # for lat, lng in zip(self.neurons[0], self.neurons[1]):
+        #     # Scaling up to real coordinates:
+        #     for i, latlng in enumerate(self.network.inputs):
+        #         actual_lat, actual_lng = latlng
+        #         if lat == actual_lat and lng == actual_lng:
+        #             visited += [i]
+        #
+        # distance = 0
+        # for index in visited:
+        #     city, prevlat, prevlng  = self.config.casemanager.original[index-1]
+        #     city, lat, lng          = self.config.casemanager.original[index]
+        #     distance += np.power((prevlat-lat), 2) + np.power((prevlng-lng), 2)
+        # total_distance = np.sqrt(distance)
+        # print("TOTAL DISTANCE: %f (optimal: %d)" % (total_distance, expected))
+        # print("Percent of optimal: %f %s" % (float(total_distance)/float(expected)*100, "%"))

kohonen_network/case_manager.py
@@ -36,16 +36,19 @@ class CaseManager():
             if not feature_independant:
                 highx = highy = max(highx, highy)
 
-            for data in dataset:
-                data[1] = data[1] / highx
-                data[2] = data[2] / highy
-            self.normalized_by = highx, highy
+            for i in range(len(dataset)):
+                dataset[i][1] = dataset[i][1] / highx
+                dataset[i][2] = dataset[i][2] / highy
+
+            config.normalized_by = (highx, highy)
             return dataset
         config.features = 2
         dataset = []
         with open(file, "r") as f:
-            cities = f.readline()
-            config.nodes = int(cities.split(":")[1])
+            name = f.readline().split(":")[1]
+            type = f.readline().split(":")[1]
+            config.nodes = int(f.readline().split(":")[1])
+            skip = f.readline().split(":")[1]
             heading = f.readline().strip("\n")
             if heading == "NODE_COORD_SECTION":
 
@@ -55,11 +58,11 @@ class CaseManager():
                     line = f.readline().strip("\n").split(" ")
                     dataset += [[int(line[0]), float(line[1]), float(line[2])]]
 
-
                 if f.readline().strip("\n") != "EOF":
                     raise Exception("Failed to interpret dataset...")
 
         if config.normalize:
+            self.original = np.copy(dataset)
             dataset = normalize(dataset, config.normalization_mode)
         return dataset
 